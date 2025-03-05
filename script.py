aed44f1f94cd3921743194cd23187aea34eabfe8
nuwan-coder1 <59862888+nuwan-coder1@users.noreply.github.com>
2025-03-05 21:11:52 +0530

    Update script.py

--- script.py
+++ script.py
@@ -104,6 +104,7 @@ def main():
         logging.info("New video detected. Updating repository variable.")
         if GITHUB_TOKEN and REPOSITORY:
             #update_repo_variable(GITHUB_TOKEN, REPOSITORY, VARIABLE_NAME, latest_video_id)
+            return None
         else:
             logging.error("GITHUB_TOKEN or GITHUB_REPOSITORY environment variables not set.")
 
